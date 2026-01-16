c876f6e5c6ad3a7e1310b0eb8aa5983d5fd2fee9
Erik Caha <erik@nowhere.com>
2026-01-16 14:21:29 +0100

    added pssession feature

--- ModuleScenario/Public/Get-OpsDiskUtil.ps1
+++ ModuleScenario/Public/Get-OpsDiskUtil.ps1
@@ -1,43 +1,82 @@
 function Get-OpsDiskUtil {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'ComputerName')]
     param (
-        [Parameter(Mandatory = $false, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Parameter(ParameterSetName = 'ComputerName', Mandatory = $false, ValueFromPipeline, ValueFromPipelineByPropertyName)]
         [Alias('Computer')]
-        [string[]]$ComputerName = $env:COMPUTERNAME
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+
+        [Parameter(ParameterSetName = 'Session', ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias('Session')]
+        [System.Management.Automation.Runspaces.PSSession[]]$PSSession
     )
 
-    process {
-        foreach ($computer in $ComputerName) {
-            try {
-                $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-                $hostname  = $computer
+    begin {
+        $diskQuery = {
+            param(
+                [string]$TargetComputer
+            )
 
-                Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computer -ErrorAction Stop |
-                    Where-Object { $_.DriveType -eq 3 } |
-                    Select-Object @{
-                        Name = "Hostname";
-                        Expression = { $hostname }
-                    }, @{
-                        Name = "Date";
-                        Expression = { $timestamp }
-                    },
-                    DeviceID,
-                    VolumeName,
-                    @{
-                        Name = "Size(GB)";
-                        Expression = { [math]::Round($_.Size / 1GB, 2) }
-                    },
-                    @{
-                        Name = "FreeSpace(GB)";
-                        Expression = { [math]::Round($_.FreeSpace / 1GB, 2) }
-                    },
-                    @{
-                        Name = "Utilization(%)";
-                        Expression = { [math]::Round((($_.Size - $_.FreeSpace) / $_.Size) * 100, 2) }
-                    }
+            $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+            $hostname  = if ($TargetComputer) { $TargetComputer } else { $env:COMPUTERNAME }
+
+            $wmiParams = @{
+                Class       = 'Win32_LogicalDisk'
+                ErrorAction = 'Stop'
             }
-            catch {
-                Write-Error "Failed to retrieve disk utilization information from $computer. Error: $_"
+            if ($TargetComputer) {
+                $wmiParams['ComputerName'] = $TargetComputer
+            }
+
+            Get-WmiObject @wmiParams |
+                Where-Object { $_.DriveType -eq 3 } |
+                Select-Object @{
+                    Name = 'Hostname'
+                    Expression = { $hostname }
+                }, @{
+                    Name = 'Date'
+                    Expression = { $timestamp }
+                },
+                DeviceID,
+                VolumeName,
+                @{
+                    Name = 'Size(GB)'
+                    Expression = { [math]::Round($_.Size / 1GB, 2) }
+                },
+                @{
+                    Name = 'FreeSpace(GB)'
+                    Expression = { [math]::Round($_.FreeSpace / 1GB, 2) }
+                },
+                @{
+                    Name = 'Utilization(%)'
+                    Expression = { [math]::Round((($_.Size - $_.FreeSpace) / $_.Size) * 100, 2) }
+                }
+        }
+    }
+
+    process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'Session' {
+                foreach ($session in @($PSSession)) {
+                    if (-not $session) { continue }
+                    try {
+                        Invoke-Command -Session $session -ScriptBlock $diskQuery -ArgumentList $null -ErrorAction Stop
+                    }
+                    catch {
+                        $sessionName = if ($session.ComputerName) { $session.ComputerName } else { $session.Name }
+                        Write-Error "Failed to retrieve disk utilization information via session '$sessionName'. Error: $_"
+                    }
+                }
+            }
+            default {
+                foreach ($computer in @($ComputerName)) {
+                    if (-not $computer) { continue }
+                    try {
+                        & $diskQuery -TargetComputer $computer
+                    }
+                    catch {
+                        Write-Error "Failed to retrieve disk utilization information from $computer. Error: $_"
+                    }
+                }
             }
         }
     }
