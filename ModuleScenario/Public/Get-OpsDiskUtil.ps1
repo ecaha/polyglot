2e5edbfd8865e519d2125919c9097e60db0295ac
Erik Caha <erik@nowhere.com>
2026-01-16 14:09:38 +0100

    added feature - iterate over the list

--- ModuleScenario/Public/Get-OpsDiskUtil.ps1
+++ ModuleScenario/Public/Get-OpsDiskUtil.ps1
@@ -1,37 +1,44 @@
 function Get-OpsDiskUtil {
-    Param (
-        [Parameter(Mandatory = $false)]
-        [Array]$ComputerName = $env:COMPUTERNAME
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias('Computer')]
+        [string[]]$ComputerName = $env:COMPUTERNAME
     )
-    try {
-        $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-        $hostname = $env:COMPUTERNAME
-        $diskUtil = Get-WmiObject -Class Win32_LogicalDisk -ComputerName $ComputerName -ErrorAction Stop |
-            Where-Object { $_.DriveType -eq 3 } |
-            Select-Object @{
-                Name = "Hostname";
-                Expression = { $hostname }
-            }, @{
-                Name = "Date";
-                Expression = { $date }
-            },
-            DeviceID,
-            VolumeName,
-            @{
-                Name = "Size(GB)";
-                Expression = { [math]::Round($_.Size / 1GB, 2) }
-            },
-            @{
-                Name = "FreeSpace(GB)";
-                Expression = { [math]::Round($_.FreeSpace / 1GB, 2) }
-            },
-            @{
-                Name = "Utilization(%)";
-                Expression = { [math]::Round((($_.Size - $_.FreeSpace) / $_.Size) * 100, 2) }
+
+    process {
+        foreach ($computer in $ComputerName) {
+            try {
+                $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+                $hostname  = $computer
+
+                Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computer -ErrorAction Stop |
+                    Where-Object { $_.DriveType -eq 3 } |
+                    Select-Object @{
+                        Name = "Hostname";
+                        Expression = { $hostname }
+                    }, @{
+                        Name = "Date";
+                        Expression = { $timestamp }
+                    },
+                    DeviceID,
+                    VolumeName,
+                    @{
+                        Name = "Size(GB)";
+                        Expression = { [math]::Round($_.Size / 1GB, 2) }
+                    },
+                    @{
+                        Name = "FreeSpace(GB)";
+                        Expression = { [math]::Round($_.FreeSpace / 1GB, 2) }
+                    },
+                    @{
+                        Name = "Utilization(%)";
+                        Expression = { [math]::Round((($_.Size - $_.FreeSpace) / $_.Size) * 100, 2) }
+                    }
             }
-        return $diskUtil
-    }
-    catch {
-        Write-Error "Failed to retrieve disk utilization information from $ComputerName. Error: $_"
+            catch {
+                Write-Error "Failed to retrieve disk utilization information from $computer. Error: $_"
+            }
+        }
     }
 }
